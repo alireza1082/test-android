ccd2fc7040a3f5747bd6e4be0cd09db740502aa9
alireza <rezaali1387@gmail.com>
2020-09-13 10:50:03 +0430

    added custom toast

--- app/src/main/java/com/example/myapplication/ToastActivity.java
+++ app/src/main/java/com/example/myapplication/ToastActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 public class ToastActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class ToastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+    }
+
+    public void toastButton(View view) {
+        //Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        Toast toast = new Toast(this);
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = inflater.inflate(R.layout.toast_layout , null);
+        toast.setView(view1);
+        toast.show();
     }
 }
